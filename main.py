3a6a9d65ae0c60e475870462b965a58dc046a85f
dreamtalen <dreamtalen@hotmail.com>
2015-08-26 11:21:41 +0800

    字典和事务的使用

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ if __name__ == '__main__':
     r = redis.StrictRedis()
     r.set('foo', 'bar')
     print r.get('foo')
+
+    r.hmset('dict', {'name': 'Bob'})
+    people = r.hgetall('dict')
+    print people
+
+    pipe = r.pipeline()
+    pipe.set('foo', 'bar')
+    pipe.get('foo')
+    result = pipe.execute()
+    print result
